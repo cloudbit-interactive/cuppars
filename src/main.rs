5c3da81b3469a0930fcda95752c69d56fcd48deb
Tufik Chediak <tufik2@gmail.com>
2021-06-03 17:16:22 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use serde_json::{Result, Value};
 
-fn main() { }
+fn main() { 
+    //println!("{}", test("Hello"))
+}
 
 pub fn json_decode(string:&str) -> Result<Value> {
     let result: Value = serde_json::from_str(string)?;
     Ok(result)
+}
+
+pub fn test(string:&str) -> &str{
+    return string;
 }
