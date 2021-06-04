85e7a106250d27f280df56dbc3a73bce477e1e74
Tufik Chediak <tufik2@gmail.com>
2021-06-03 21:35:33 -0400

    update changes

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,6 @@
-use serde_json::{Result, Value};
+use cuppa::{json_decode, test};
 
 fn main() { 
-    //println!("{}", test("Hello"))
-}
-
-pub fn json_decode(string:&str) -> Result<Value> {
-    let result: Value = serde_json::from_str(string)?;
-    Ok(result)
-}
-
-pub fn test(string:&str) -> &str{
-    return string;
+    let result = json_decode(r#"{"name":"Tufik", "age":99}"#).unwrap();
+    println!("{:?} {}", result["name"], test("Hello Test"));
 }
