39232079ee8c49fac0e671bf2055d0fdaa00a1f5
Tufik Chediak <tufik2@gmail.com>
2021-07-14 16:11:46 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,26 +9,17 @@ struct User{
     name:String,
 }
 
-/*
-#[derive(Debug)]
-enum Result<T, E> {
-    Ok(T),
-    Err(E),
-}
-fn get_word_count_from_file(file_path:&str) -> Result<u32, &'static str>{
-    if file_path == "" {
-        return Result::Err("File can not be found!");
-    }
-    println!("\nget_word_count_from_file {}", file_path);
-    Result::Ok(20)
-}
-*/
-
-fn main() { 
+fn main_async() -> Result<()>{
+    
     let db = DataBase::new("localhost", 3306, "rust", "root", "");
     let row = db.get_row("users".to_string(), "id = 1".to_string(), "".to_string(), "".to_string());
-    if row.is_err() { return; }
+    if row.is_err() { return Ok(()); }
     let row = row.unwrap();
-    let user = || -> Result<User>{ Ok( serde_json::from_str( &json!(row).to_string())? ) }().unwrap();
+    let user:User = serde_json::from_value(json!(row))?;
     println!("{:?}", user);
+    Ok(())
+}
+
+fn main() { 
+    main_async().unwrap();
 }
